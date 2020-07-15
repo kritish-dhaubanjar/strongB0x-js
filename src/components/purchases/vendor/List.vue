bc9beeb43ee2e2969bdd21f93bcbe75f5adf3adc
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-07-15 18:30:46 +0545

    Edit Customer -> Edit Vendor

--- src/components/purchases/vendor/List.vue
+++ src/components/purchases/vendor/List.vue
@@ -67,7 +67,7 @@
     <v-dialog v-model="edit.edit" max-width="768px">
       <v-card>
         <v-card-title>
-          <span class="headline">Edit Customer</span>
+          <span class="headline">Edit Vendor</span>
         </v-card-title>
 
         <v-card-text>
